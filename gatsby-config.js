be55c88226d1ccf60016a08c1807455b87e7dea7
Rakib <rakibpaucse@gmail.com>
2020-12-16 19:58:42 +0600

    final

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,11 +2,8 @@ module.exports = {
   siteMetadata: {
     title: `Primeasia University`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-<<<<<<< HEAD
+
     author: `Nazrul Islam`,
-=======
-    author: `Wahid`,
->>>>>>> 26503aac3ff367c5c4343f6df3da1cda9f7927b6
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -28,7 +25,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/logo.png`, // This path is relative to the root of the site.
       },
     },
     {
